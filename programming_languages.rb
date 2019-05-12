ff6f87de1c7c797a49a1a856d8aec5f709445ef4
moniet <moniet.sawhney@gmail.com>
2019-05-12 18:31:38 +0100

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
+
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |type, data|
+    data.each do |lang, val| # binding.pry
+      if lang == :javascript
+        new_hash[lang] = val
+        new_hash[lang][:style] = [:oo, :functional]
+      else
+       new_hash[lang] = val
+       new_hash[lang][:style] = [type]
+      end
+    end
+  end
+  new_hash
 end
+
+#reformat_languages(languages)
